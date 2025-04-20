718bae9186f389785fadcceca1bdd33d30ddf7e9
Jacques Pharand <jacques.pharand@proton.me>
2025-04-19 23:11:46 -0400

    Solve circular import embarassment

--- tenable_api/__init__.py
+++ tenable_api/__init__.py
@@ -7,14 +7,6 @@ A wrapper around the tenable.com public API.
 __version__ = "0.0.1"
 
 
-from tenable_api.attack_path_techniques import AttackPathTechniques
-from tenable_api.audits import Audits
-from tenable_api.cve import CVE
-from tenable_api.indicators import Indicators
-from tenable_api.plugins import Plugins
-from tenable_api.plugins import query
-
-
 class QueryParamter():
     """
     Base class for all query parameters
